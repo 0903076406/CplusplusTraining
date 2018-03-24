128ba42104b00a2b3ae505d5cd9b5a105ded9138
Lam Tran <tranthanhlam@gmail.com>
2018-03-25 00:52:02 +0700

    fix display info

--- constructor.cpp
+++ constructor.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 class Empty {
 public:
-    Empty() {cout<<"Default Copy Constructor"<<endl;}
+    Empty() {cout<<"Defaul Constructor"<<endl;}
     
     Empty (const Empty &rhs) {cout <<"default copy constructor"<<endl;}
     ~Empty() {cout <<"Default deconstructor"<<endl;}
@@ -13,6 +13,10 @@ public:
 
 };
 
+void foo(Empty e)
+{
+    cout <<"Empty foo"<<endl;
+}
 int main(void)
 {
     cout <<"Empty e1" << endl;
@@ -23,6 +27,8 @@ int main(void)
     Empty e3 = e1;
     cout << "Empty e2 = e1"<< endl;
     e2 = e1;
+    cout << "foo get called"<< endl;
+    foo(e1);
     cout << "Exit ..." << endl;
     return 0;
 }
